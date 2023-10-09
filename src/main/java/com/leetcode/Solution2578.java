350e91c32e331894370c568861531904507b94f6
liuqi <date20150619@icloud.com>
2023-10-09 22:37:30 +0800

    最小和分割

---
+++ src/main/java/com/leetcode/Solution2578.java
@@ -0,0 +1,67 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution2578 {
+    List<Integer> numList;
+    List<Integer> numList1;
+    List<Integer> numList2;
+
+    public Solution2578() {
+        numList = new ArrayList<>();
+        numList1 = new ArrayList<>();
+        numList2 = new ArrayList<>();
+    }
+
+
+    public int splitNum(int num) {
+        while (num != 0) {
+            numList.add(num % 10);
+            num = num / 10;
+        }
+        numList.sort(null);
+
+        int startIndex = binarySearchNotZero(numList);
+        numList = numList.subList(startIndex, numList.size());
+
+        if (numList.size() == 1) {
+            return numList.get(0);
+        }
+
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < numList.size(); i++) {
+            if (i % 2 == 0) {
+                numList1.add(numList.get(i));
+                sb1.append(numList.get(i));
+            } else {
+                numList2.add(numList.get(i));
+                sb2.append(numList.get(i));
+            }
+        }
+        int num1 = Integer.parseInt(sb1.toString());
+        int num2 = Integer.parseInt(sb2.toString());
+        return num1 + num2;
+    }
+
+    public int binarySearchNotZero(List<Integer> numList) {
+        int low = 0;
+        int high = numList.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (numList.get(mid) == 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        Solution2578 solution2578 = new Solution2578();
+        System.out.println(solution2578.splitNum(1001));
+    }
+}
